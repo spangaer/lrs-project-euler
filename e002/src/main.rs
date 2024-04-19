3a2960a0ecf54510331a137dda0d4c6853a7b264
Jean-Luc Deprez <spangaer@users.noreply.github.com>
2024-04-19 15:39:04 +0000

    Problem 2

---
+++ e002/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    const UPPER_BOUND: i64 = 4_000_000;
+
+    let generator = {
+        let mut prev: i64 = 1;
+        let mut last: i64 = 1;
+
+        std::iter::from_fn(move || {
+            let next = prev + last;
+            prev = last;
+            last = next;
+            Some(next)
+        })
+    };
+
+    let res: i64 = generator
+        .take_while(|x| *x <= UPPER_BOUND)
+        .filter(|x| x % 2 == 0)
+        .sum();
+    println!("{}", res);
+}
