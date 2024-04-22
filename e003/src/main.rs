6e7a936b461833a1c1636451e02c4d7e9ff8e144
Jean-Luc Deprez <spangaer@users.noreply.github.com>
2024-04-22 19:25:39 +0000

    Problem 3

---
+++ e003/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let mut primes = e_math::generate::Primes::new();
+
+    let start: u128 = 600_851_475_143;
+
+    let iter = primes.iterator();
+
+    let res = iter
+        .scan(start, |current, prime| match current {
+            current if *current == prime => None, // current state is a prime
+            current if *current % prime == 0 => {
+                // current state was prime dividable
+                let new = *current / prime;
+                *current = new; // write back internal state
+                eprintln!("{}", new);
+                Some(new)
+            }
+            current => Some(*current), // just repeat
+        })
+        .last() //when iterator ends result will have been in last element
+        .unwrap();
+
+    println!("{}", res); //6857
+}
