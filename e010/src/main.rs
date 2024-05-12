dd5b92650c96f2ad7abf7f9309a05807f4f4237b
Jean-Luc Deprez <spangaer@users.noreply.github.com>
2024-05-12 12:40:08 +0000

    Problem 10

---
+++ e010/src/main.rs
@@ -0,0 +1,12 @@
+use e_math::generate::Primes;
+
+fn main() {
+    let mut primes = Primes::<u64>::new();
+
+    let res = primes
+        .iterator()
+        .take_while(|&p| p < 2_000_000_u64)
+        .sum::<u64>();
+
+    println!("{}", res); // 142913828922
+}
