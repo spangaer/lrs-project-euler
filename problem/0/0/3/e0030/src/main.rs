e4bdb0e35b909a17655e03230b9b76ae5611c263
Jean-Luc Deprez <spangaer@users.noreply.github.com>
2024-06-28 08:39:28 +0000

    Problem 30 with smart bound

--- problem/0/0/3/e0030/src/main.rs
+++ problem/0/0/3/e0030/src/main.rs
@@ -1,26 +1,47 @@
+use std::iter;
+
 use e_math::num::digits;
 
 fn main() {
-    (11..=100000)
-        .map(|i| (i, sum_of_digit_powers(i, 4)))
-        .filter(|(i, s)| i == s)
-        .for_each(|(i, s)| println!("{i} {s}"));
+    for p in 2..=6 {
+        println!();
+        let res: u64 = equal_sum_of_digit_powers_series(p)
+            .map(|i| {
+                println!("{i}");
+                i
+            })
+            .sum();
 
-    println!();
+        println!();
+        println!("{p}   {res}");
+        println!();
+    }
 
-    let res: u64 = (11..=1_000_000)
-        .map(|i| (i, sum_of_digit_powers(i, 5)))
-        .filter(|(i, s)| i == s)
-        .map(|(i, _)| {
-            println!("{i}");
-            i
-        })
-        .sum();
-
-    println!();
-    println!("{res}") // 443839
+    // 3   1301
+    // 4   19316
+    // 5   443839
+    // 6   548834
 }
 
-fn sum_of_digit_powers(i: u64, pow: u32) -> u64 {
-    digits(&i).map(|d| (d as u64).pow(pow)).sum()
+fn sum_of_digit_powers(i: u64, pow: u32) -> (u64, usize) {
+    let collected: Vec<_> = digits(&i).map(|d| (d as u64).pow(pow)).collect();
+
+    (collected.iter().sum(), collected.len())
+}
+
+fn equal_sum_of_digit_powers_series(pow: u32) -> impl Iterator<Item = u64> {
+    let mut i = 1;
+    iter::from_fn(move || {
+        i += 1;
+        let (sum, len) = sum_of_digit_powers(i, pow);
+
+        // when the digit-len * 9^pow drops below 'i', we can't reach i anymore so we can stop
+        // add + 1 just for certainty of not cutting off too soon (digit increase verge)
+        if i <= (len as u64 + 1) * 9_u64.pow(pow) {
+            Some((i, sum))
+        } else {
+            None
+        }
+    })
+    .filter_map(|(i, s)| if i == s { Some(i) } else { None })
 }
