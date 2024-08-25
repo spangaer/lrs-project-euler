46617f70838440ea5eb32f7347c700ba8032daf1
Jean-Luc Deprez <spangaer@users.noreply.github.com>
2024-08-25 14:45:12 +0000

    Prepare problem 32

---
+++ problem/0/0/3/e0032/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // user itertools to generate combinations of set 1 to 9, from 1 to 8, remaining  -1.
+    // Then test if product digits are destinct from the two parts
+    // Don' forget to test for unique numbers.
+
+    println!("Hello, world!");
+}
